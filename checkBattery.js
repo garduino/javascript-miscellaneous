42f876d667aac4e46210db1e1e6605a2771b9f56
garduino <gsa@arsol.net>
2015-08-13 10:36:28 -0300

    Battery and checkBattery (still not working).

---
+++ checkBattery.js
@@ -0,0 +1,19 @@
+// Get battery status as soon as it's available
+Battery.getStatus(function(status, error) {
+  if(error) {
+    console.error('Battery status is not supported');
+    return;
+  }
+  console.log('Level: ' + Math.floor(status.level * 100) + '%'); // 30%
+  console.log('Charging: ' + status.charging);                   // true
+  console.log('Time until charged: ' + status.chargingTime);     // 3600 (seconds) or Infinity
+  console.log('Battery time left: ' + status.dischargingTime);   // 3600 (seconds) or Infinity
+});
+
+// Register a handler to get notified when battery status changes
+Battery.onUpdate(function(status) {
+  console.log('Level: ' + Math.floor(status.level * 100) + '%'); // 30%
+  console.log('Charging: ' + status.charging);                   // true
+  console.log('Time until charged: ' + status.chargingTime);     // 3600 (seconds) or Infinity
+  console.log('Battery time left: ' + status.dischargingTime);   // 3600 (seconds) or Infinity
+});
