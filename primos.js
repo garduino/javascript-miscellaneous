e994a0e30f79bd4c4ed07e7aead7917dd6c6af12
German Arduino <garduino@gmail.com>
2015-08-15 13:13:12 -0300

    Return if a passed argument is a primer number or not
    Uses the primality package for Nodejs

---
+++ primos.js
@@ -0,0 +1,10 @@
+// Return if a number is primer or not
+// Call as: node primos 1 3 5 9 30 44 67 88 95
+// Requiere install of primality package as node install primality
+
+var primality = require('primality');
+
+process.argv.forEach(function (val, index, array) {
+  console.log(val + ' isPrime ' + primality(val));
+});
+
