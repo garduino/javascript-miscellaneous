7338f55e1553bfacf9545c83b3dce7674cdd8ce7
German Arduino <garduino@gmail.com>
2015-08-16 19:31:55 -0300

    Project Euler Problem 7.

---
+++ euler7.js
@@ -0,0 +1,19 @@
+// Return the 10001 st primer number.
+// Requiere install of primality package as node install primality
+
+var primality = require('primality');
+var n = 0;
+
+for (var i=1; i<200000; i++) {
+	
+	if (primality(i)) {
+		var n = n + 1;
+		
+		if (n == 10001) {
+		console.log(i + ' is 10001st prime number!' );
+			}
+    	}
+    	// console.log(i);
+}
+
+
