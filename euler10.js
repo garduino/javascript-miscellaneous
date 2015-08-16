a4eaa7ebf66194afa205f87f7f20000195f639bc
German Arduino <garduino@gmail.com>
2015-08-16 19:38:23 -0300

    Project Euler Problem 10.

---
+++ euler10.js
@@ -0,0 +1,14 @@
+// Find the sum of all the primes below two million.
+// Requiere install of primality package as node install primality
+
+var primality = require('primality');
+var n = 0;
+
+for (var i=1; i<2000000; i++) {
+	
+	if (primality(i)) {
+		var n = n + i;
+   	}
+}
+console.log('The sum of all the primes below two millons is ' + n);
+
