678009cf7bd37ec9a0fd50af7c34080fcc9a809e
German Arduino <garduino@gmail.com>
2017-07-13 20:20:37 -0300

    Fixed prodCode and appCode.

--- valic.js
+++ valic.js
@@ -2,7 +2,7 @@ var licenseKey = require('licensekey');
 
 var userInfo = {company:"miempresa.com",street:"Avellaneda 399", city:"Rosario", state:"Santa Fe", zip:"2000"}
 
-var userLicense = {info:userInfo, prodCode:"LEN100120", appVersion:"1.5", osLock:true, license:"7130-2074-776U-TY33-6431-415E-770C-6DD"}
+var userLicense = {info:userInfo, prodCode:"NOLSU100", appVersion:"1.0", osLock:true, license:"XU0W7-YB63F-06UTV-F8D56-4FB86-F8EC7"}
  
 try{
     var license = licenseKey.validateLicense(userLicense);
