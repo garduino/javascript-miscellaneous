579ab2b312e899ca9c105a9f0384ded59ff3f85f
Arduino German <GArduino@sancorseguros.com>
2017-07-21 13:07:58 -0300

    Trying to reload a locki db.

---
+++ locki2.js
@@ -0,0 +1,18 @@
+var loki = require('lokijs');
+  // db = new loki('test.json'),
+
+// var db2 = new loki('test.json');
+
+var db2 = new loki('test.json', {
+	autoload: true,
+	// autoloadCallback : databaseInitialize,
+	autosave: true, 
+	autosaveInterval: 4000
+});
+
+// console.log(db2.data);
+
+db2.loadDatabase({}, function () {
+  var users2 = db2.getCollection('users')
+  console.log(db2.data);
+});
