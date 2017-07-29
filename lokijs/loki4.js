09c13d0c5e21b4a724e8652c3449a8a31b6c9b91
German Arduino <garduino@gmail.com>
2017-07-29 14:25:37 -0300

    Fixes in loki examples.

---
+++ lokijs/loki4.js
@@ -0,0 +1,18 @@
+
+// Create a datastore
+var loki = require('lokijs'), db = new loki('testdr.db');
+
+// Create a collection
+var doctors = db.addCollection('doctors');
+
+// Insert some records
+doctors.insert({ name: 'David Tennant', doctorNumber: 10 }); 
+doctors.insert({ name: 'Matt Smith', doctorNumber: 11 });
+
+// Create a dynamic view
+var view = doctors.addDynamicView('newerDoctors'); 
+view.applyWhere(function (obj) { return obj.doctorNumber > 8; });
+view.applySimpleSort('doctorNumber', true);
+
+console.log('*************************** Ahora view.data *****************************')
+view.data();
