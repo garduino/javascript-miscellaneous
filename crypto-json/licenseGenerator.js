839fb651679d7d45b767f4408a87be3a9bc4eebb
German Arduino <garduino@gmail.com>
2017-07-31 23:15:59 -0300

    licenseGenerator tries.

--- crypto-json/licenseGenerator.js
+++ crypto-json/licenseGenerator.js
@@ -7,37 +7,14 @@ var encoding = 'hex'
 var license = {
   company_name: 'Proyección ART',
   company_mail: 'sperez@proyeccionart.com.ar',
-  licenses: 10,
   due_date: '31/12/2017',
-  machines: [
-    {uuid: '', registration_date: ''},
-    {uuid: '', registration_date: ''},
-    {uuid: '', registration_date: ''},
-    {uuid: '', registration_date: ''},
-    {uuid: '', registration_date: ''},
-    {uuid: '', registration_date: ''},
-    {uuid: '', registration_date: ''},
-    {uuid: '', registration_date: ''},
-    {uuid: '', registration_date: ''},
-    {uuid: '', registration_date: ''}
-  ]
 }
  
 var encrypted = cryptoJSON.encrypt(license, passKey, {
   algorithm: cipher,
   encoding: encoding,
   // En keys van las que NO se encriptan
-  keys: ['machines']
+  // keys: ['machines']
 })
  
 console.dir(encrypted) // => 
- 
-/*
-  { first_name: 'ac64e6168ebbb8c575a567fa4bdd467c',
-    last_name: 'Davis',
-    instrument: 'Trumpet',
-    birth_year: '735f22844209a3dea04f2c070ead7c5b',
-    albums:
-     [ { title: 'Birth of the Cool', year: 1957 },
-       { title: 'Bitches Brew', year: 1970 } ] }
-*/
