e1b6037e6aa112c5dabb5ededfe1706720bd7a27
GArduino <garduino@gmail.com>
2017-09-20 15:18:13 -0300

    Intermediate Save.

--- licensing/node-serial-key1.js
+++ licensing/node-serial-key1.js
@@ -1,11 +1,19 @@
 var serial = require('node-serial-key');
 
-var deviceserial = serial.getSerial(function(err, output) {
+var out1 = serial.getSerial(function(err, output) {
     //// GET SERIAL NUMBER ON DEVICE ////
     console.log('Output: ' + output);
-    console.log('Device Serial: ' + deviceserial);	
+    var out2 = output;
 })
 
+console.log(out1);
+
 // Generar un serial para proteger la app
-var genSerial = serial.generateSerial(deviceserial, 'yoursecrethere');
+// var genSerial = serial.generateSerial('WD-WMC2E9342069', 'yoursecrethere');
+var genSerial = serial.generateSerial(out2, 'yoursecrethere');
+console.log('Output 2: ' + out2);
 console.log('Serial: ' + genSerial);
+
+
+// var yourGenerateKey = genSerial;
+
